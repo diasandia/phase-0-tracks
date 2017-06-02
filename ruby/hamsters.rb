7b32f144d9126026efd36173ecf950e5c4601dc9
Diana <diasandia@users.noreply.github.com>
2017-06-01 23:47:16 -0700

    Initial commit

---
+++ ruby/hamsters.rb
@@ -0,0 +1,15 @@
+puts "What is the hamster's name?"
+hamster_name=gets.chomp
+
+puts "From 1 to 10 (10 being the loudest), what is the hamster's volume level?"
+hamster_volume=gets.to_i
+
+puts "What is the hamster's fur color?"
+hamster_furcolor=gets.chomp
+
+puts "Is the hamster a good candidate for adoption? (yes/no)"
+hamster_adoptable=gets.chomp
+
+puts "What is the hamster's approximate age?"
+hamster_estage=gets.chomp
+
