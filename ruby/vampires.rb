0c5af749d59d3dd3670bd1fd337e816f9904fe39
Diana <diasandia@users.noreply.github.com>
2017-06-03 12:49:25 -0700

    Add questions to determine if new employee is a vampire to vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,16 @@
 #4.4 Solo Challenge: Control Flow Practice
 
+#Gather data on new employee
 
+#ask for name
+puts "What's your name?"
+
+#ask for age and year of birth
+puts "How old are you?"
+puts "Where year were you born?"
+
+#ask individual if they would like garlic bread
+puts "Would you like garlic bread?"
+
+#ask if they would like to enroll in the company's health insurance
+puts "Would you like to enroll in the company's health insurance?"
