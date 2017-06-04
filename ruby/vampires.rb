dc127075068ecac8320b6640e0faa8c58e67a018
Diana <diasandia@users.noreply.github.com>
2017-06-04 13:21:30 -0700

    Add plot twist message of lets all be friends

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,25 +2,25 @@
 
 # #Gather data on new employee
 
-# #ask how many employees will be in the process
+#ask how many employees will be in the process
 puts "How many employees will be in the process?"
   employees=gets.chomp.to_i
   until employees == 0
 
-# #ask for name
+#ask for name
 puts "What's your name?"
   name=gets.chomp
 
 #ask for age and year of birth
 puts "How old will you be in 2017? (ex. 96)"
-age=gets.to_i
+  age=gets.to_i
 while age == 0
   puts "Invalid value. Please use a number. How old will you be in 2017?"
   age=gets.to_i
 end
 
 puts "What year were you born? (ex.1920)"
-year=gets.to_i
+  year=gets.to_i
 while year == 0
 puts "Invalid value. Please use a number. What year were you born?"
 year=gets.to_i
@@ -35,7 +35,7 @@ end
 
 #ask individual if they would like garlic bread
 puts "Would you like garlic bread? (yes/no)"
-garlic=gets.chomp.downcase
+  garlic=gets.chomp.downcase
 while garlic != "yes" && garlic != "no"
   puts "Invalid value. Please note yes or no. Would you like garlic bread?"
   garlic=gets.chomp
@@ -48,7 +48,7 @@ end
 
 #ask if they would like to enroll in the company's health insurance
 puts "Would you like to enroll in the company's health insurance? (yes/no)"
-insurance=gets.chomp.downcase
+  insurance=gets.chomp.downcase
 while insurance != "yes" && insurance != "no"
   puts "Invalid value. Please note yes or no. Would you like to enroll in the company's health insurance?"
   insurance=gets.chomp
@@ -61,12 +61,11 @@ end
 
 #ask if they have any allergies
 puts "Please list any allergies. Enter one allergy at a time. When finish type done."
-allergies=gets.chomp
+  allergies=gets.chomp
 while allergies!= "sunshine" && allergies!="done"
   puts "Any other allergies? Please enter one allergy at a time. When finish type done."
   allergies=gets.chomp
 end
-
 
 case
   when name== "Drake Cula" || name== "Tu Fang"
@@ -84,6 +83,8 @@ end
 puts "Employee:: #{name} - #{vamp_stat}. Conclusion is based on following interview questions and answers - Age: #{age}; Year of birth: #{year}; Likes garlic bread: #{garlic}; Has health insurance: #{insurance}."
 
 employees -= 1
-  end
+end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
 
