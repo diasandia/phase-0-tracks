bee7e647bc8559dfe838bc6a8e18aa9be40cc4c6
Diana <diasandia@users.noreply.github.com>
2017-06-08 15:14:32 -0700

    Add a simple search and calculate fibonacci numbers per release 0 & 1

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,54 @@
 #5.6 Mandatory Pairing: Solving Problems with Data Structures
 
+## RELEASE 0
+def search_array(arr,num)
+  counter = 0
 
+ arr.each do |item|
+    if item != num
+      counter += 1
+    elsif item == num
+      break
+    end
+
+   if counter == arr.length
+      counter = nil
+    end
+  end
+
+ counter
+end
+
+arr = [1, 42, 89, 23, 45]
+p search_array(arr, 45)
+
+# RELEASE 1
+
+def fib(num)
+  array = [0,1]
+
+ if num == 0
+    array = [0]
+  elsif num == 1
+    array = [0,1]
+  else
+    pos = 2
+    until array.length == num
+      array[pos] = array[pos-2] + array[pos-1]
+      pos += 1
+    end
+  end
+
+ if array.length > 10
+    array.last
+  else
+    array
+  end
+
+end
+
+p fib(0)
+p fib(1)
+p fib(2)
+p fib(6)
+p fib(100)
