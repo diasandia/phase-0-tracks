c2a88a0eed90eda7aa5f595e34dcf94904e95c5c
Diana <diasandia@users.noreply.github.com>
2017-06-08 16:18:29 -0700

    Add pseudocode for interior designer program

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,1 +1,12 @@
 #5.2 Arrays and Hashes
+
+#PROGRAM REQUIREMENTS (pseudocode)
+# Allow interior designer to enter client details (name, age, number of children, pets, decor theme)
+# 1. Prompt designer/user for info
+# 2. Convert user input to appropriate data type
+# 3. Print hash back out to the screen when the designer answers all the questions
+# 4. Give the user opportunity to update key (no loop needed)
+    # A. If designer says "none" skip this
+    # B. If designer says "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.)
+#5. Print latest version of the hash and exit the program
+
