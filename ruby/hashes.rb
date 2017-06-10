4b0e91ca435c40b782689fa748cda613e981ff8e
Diana <diasandia@users.noreply.github.com>
2017-06-09 17:22:29 -0700

    Add questions to prompt designer for clients info and set up hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,8 +1,61 @@
 #5.2 Arrays and Hashes
 
-#PROGRAM REQUIREMENTS (pseudocode)
+#PROGRAM REQUIREMENTS
 # Allow interior designer to enter client details (name, age, number of children, pets, decor theme)
-# 1. Prompt designer/user for info
+
+#Set up hash
+ client_info = {
+    "name" => "",
+    "age" => "",
+    "number_children" => "",
+    "pets" => "",
+    "decor_style" => ""
+}
+
+# Prompt designer for client's info
+  puts "Please provide the client's name: "
+  client_info["name"] = gets.chomp
+
+  puts "Client's age:"
+  client_info["age"] = gets.chomp.to_i
+
+  puts "Number of children"
+  client_info["number_children"] = gets.chomp.to_i
+
+  puts "Do you have a dog, cat, both, or none? (dog/cat/both/none)"
+  pets = gets.chomp.downcase
+    if pets == "dog" || pets == "cat" || pets == "both"
+     client_info["pets"] = true
+    elsif
+      pets == "none"
+      client_info["pets"] = false
+    else
+      puts "Invalid value. Do you have a dog, cat, both, or none? (dog/cat/both/none)"
+      pets = gets.chomp.downcase
+    end
+
+  puts "Select a decor theme: farm/victorian/craftsman"
+  decor_style = gets.chomp.downcase
+    if decor_style == "farm"
+     client_info["decor_style"] = "farm"
+    elsif decor_style == "victorian"
+     client_info["decor_style"] = "victorian"
+    elsif decor_style == "craftsman"
+     client_info["decor_style"] = "craftsman"
+    else
+      puts "invalid value.Select a decor style: farm/victorian/craftsman "
+      decor_style = gets.chomp.downcase
+    end
+
+#Print hash out back to designer
+p client_info
+
+puts " Is there anything you would like to update?"
+
+
+
+### Pseudocode
+# 1. Prompt designer for info
 # 2. Convert user input to appropriate data type
 # 3. Print hash back out to the screen when the designer answers all the questions
 # 4. Give the user opportunity to update key (no loop needed)
