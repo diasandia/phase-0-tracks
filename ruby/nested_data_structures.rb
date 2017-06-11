c28127daf733f713e0e46ad188883ce575ded60e
Diana <diasandia@users.noreply.github.com>
2017-06-11 01:24:09 -0700

    Add and design a nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -4,3 +4,47 @@
 
 #Design and build a nested data structure. Use mix of hashes and arrays.
 
+gym = {
+    yoga_room: {
+        class_name: "Vinyasa Yoga",
+        class_info: {
+            student_level: "Intermediate",
+            total_seats: 60,
+            seats_available: 9
+        },
+        equipment: [
+            "Yoga Mats",
+            "Blocks",
+            "Straps"
+        ]
+    },
+
+    spin_room: {
+        class_name: "Indoor Cycling",
+        class_info: {
+            student_level: "Beginner",
+            total_seats: 30,
+            seats_available: 5
+        },
+        equipment: [
+            "Stationary Bike"
+        ]
+    },
+
+    weight_room: {
+        class_name: "Strength Training",
+        class_info: {
+            student_level: "Advance",
+            total_seats: 20,
+            seats_available: 1
+        },
+        equipment: [
+            "Barbell",
+            "Bench Press",
+            "Squat Rack"
+        ]
+    }
+}
+
+
+
