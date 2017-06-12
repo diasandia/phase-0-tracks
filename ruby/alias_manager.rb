98a8778d07c94de975ea5d46ffca7962fd7413df
Diana <diasandia@users.noreply.github.com>
2017-06-11 23:07:46 -0700

    Complete solo challenge by adding print section at the bottom

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,43 +1,47 @@
 #5.5 Solo Challenge - Manipulating Strings
 
-# RELEASE 0
 # Get name from user
 # Swap the first and last name
 # Change all the vowels (a,e,i,o,u) to the next vowel
 # Change the consonants to the next consonants
 # Print new alias name
 
-puts "What's your first name?"
-  first_name = gets.chomp.downcase.split('')
 
-puts "What's you last name?"
-  last_name = gets.chomp.downcase.split('')
+#Get name from user
+      puts "What's your first name?"
+        og_first_name = gets.chomp
+        first_name = og_first_name.downcase.split('')
 
-vowels = ['a', 'e', 'i', 'o','u']
-consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
-index = 0
+      puts "What's you last name?"
+        og_last_name = gets.chomp
+        last_name = og_last_name.downcase.split('')
 
-alias_first_name = first_name.map do |letters|
-  if vowels.include? (letters)
-      letters = vowels.rotate(1)[vowels.index(letters)]
-  else consonants.include? (letters)
-      letters = consonants.rotate(1)[consonants.index(letters)]
-  end
-end
+      vowels = ['a', 'e', 'i', 'o','u']
+      consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+      index = 0
 
-alias_last_name = last_name.map do |letters|
-  if vowels.include? (letters)
-      letters = vowels.rotate(1)[vowels.index(letters)]
-  else consonants.include? (letters)
-      letters = consonants.rotate(1)[consonants.index(letters)]
-  end
-end
+#Change vowels and consonants
+        alias_first_name = first_name.map do |letters|
+          if vowels.include? (letters)
+              letters = vowels.rotate(1)[vowels.index(letters)]
+          else consonants.include? (letters)
+              letters = consonants.rotate(1)[consonants.index(letters)]
+          end
+        end
 
-alias_name= "#{alias_last_name.join("").capitalize} #{alias_first_name.join("").capitalize}"
+        alias_last_name = last_name.map do |letters|
+          if vowels.include? (letters)
+              letters = vowels.rotate(1)[vowels.index(letters)]
+          else consonants.include? (letters)
+              letters = consonants.rotate(1)[consonants.index(letters)]
+          end
+        end
 
-p alias_name
+#Print new alias name
+      alias_name= "#{alias_last_name.join("").capitalize} #{alias_first_name.join("").capitalize}"
+puts "Your new alias is #{alias_name}"
 
-
+puts "#{alias_name} is actually #{og_first_name.capitalize} #{og_last_name.capitalize}."
 
 
 #--------------------------------------------------------------------------------------
