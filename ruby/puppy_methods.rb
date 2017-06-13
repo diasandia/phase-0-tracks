6d57188fd2a7d51da207e5e96cf131da091b6e53
Diana <diasandia@users.noreply.github.com>
2017-06-13 13:51:46 -0700

    Add release 2 to complete pairpro 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -28,8 +28,6 @@ class Puppy
 
 end
 
-
-
 fido = Puppy.new
 fido.fetch("ball")
 fido.speak(3)
@@ -37,3 +35,35 @@ fido.rollover
 fido.dog_years(2)
 fido.high_five
 
+class Car
+
+  def initialize
+    puts "Initializing new car instance ..."
+  end
+
+  def car_manufacture(make,model)
+    puts "The car is made by #{make} and the #{model}"
+  end
+
+  def energy_efficient(type)
+    if type.downcase== "electric" || type.downcase == "hybrid"
+      puts "The car is energy efficient"
+    else
+      puts "The care is not energy efficient. :("
+    end
+  end
+
+end
+
+car_array = []
+index = 0
+until index == 50
+  car_array[index] = Car.new
+  index += 1
+end
+
+
+car_array.each do |x|
+  x.car_manufacture("Kia", "Sportage")
+  x.energy_efficient("hybrid")
+end
