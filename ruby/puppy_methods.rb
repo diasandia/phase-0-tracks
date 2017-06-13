74bd2cfc0bca336d43b38fba0b4dadf7ea0bfb1b
Diana <diasandia@users.noreply.github.com>
2017-06-13 12:28:21 -0700

    Complete release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,31 @@ class Puppy
     toy
   end
 
+  def speak(i)
+    i.times { puts "woof"}
+  end
+
+  def rollover
+    p "*rollover*"
+  end
+
+  def dog_years(human_years)
+    dog_years = 7 * human_years
+    puts dog_years
+  end
+
+  def high_five
+    puts "paw"
+  end
+
 end
+
+
+
+fido = Puppy.new
+fido.fetch("ball")
+fido.speak(3)
+fido.rollover
+fido.dog_years(2)
+fido.high_five
 
