8cab42634b5986e619faa6ccdda504386b2b5ca8
Diana <diasandia@users.noreply.github.com>
2017-06-13 17:26:23 -0700

    Design a class for release 0

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,1 +1,25 @@
 #6.1 Introduction to Classes
+
+# Release 0 - Deign a class
+
+  # Puppy ------------------- #[Class]
+  # Australian shepherd
+
+  # Characteristics ---------------
+  # Temperament: varies
+  # Personality: varies
+  # Colors: varies
+  # Sizes: 3
+  # Height: varies
+  # Wight: varies
+
+  # Behavior ---------------
+  # Clingy
+  # Work oriented
+  # Vocal
+  # Sits
+  # Jump
+
+
+
+
