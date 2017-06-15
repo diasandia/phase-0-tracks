c0ad4d49f62712e594ecfed2608a01b572a782ca
Diana <diasandia@users.noreply.github.com>
2017-06-15 13:45:04 -0700

    Refactor with attr_reader and attr_accessor per release 3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,13 @@
 #6.3 Attributes
 
 class Santa
-  def initialize (gender, eye_color, hair_color)
+  attr_reader :ethnicity, :reindeer_ranking
+  attr_accessor :gender
+
+  def initialize (gender, ethnicity)
     puts "Initializing Santa instance ..."
     @gender = gender
-    @eye_color = eye_color
+    @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
   end
@@ -17,23 +20,55 @@ class Santa
     puts "That was a good #{cookie_type}"
   end
 
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer_name)
+   @reindeer_ranking.delete(reindeer_name)
+   @reindeer_ranking.push(reindeer_name)
+  end
 end
 
-santas = []
-biological_gender = ["female", "male", "decline to state", "why do you care"]
-eye_color = ["blue", "green", "brown", "black"]
-hair_color = ["brown", "black", "white", "pink", "blue"]
+# GETTER METHODS for attributes: methods that wrap around a piece of data and return that piece of data. Make private data publicly available outside of class. **They are readable [Shortcut:: attr_reader :gender, :ethnicity, :reindeer_ranking. Declare attr_reader at the top. Think of attr_reader as saying write me a getter. They are great because other programmers will know what is readable or writable and know what kind of access they can have outside the class]
 
-biological_gender.length.times do |i|
-  santas << Santa.new(biological_gender[i], eye_color[i], hair_color[i])
-end
+      # def gender
+      #   @gender
+      # end
+
+      # def ethnicity
+      #   @ethnicity
+      # end
+
+      # def reindeer_ranking
+      #   @reindeer_ranking
+      # end
+
+# SETTER METHODS: Makes an attribute writable. FYI -depending on the class, you may not want to make something writable ex. location.  Allows for attributes to be set from outside the class instead of inside the class.[Shortcut:: attr_accessor :gender. Declare attr_accessor at the top. Think of attr_accessor as saying write me a setter method. Attr_accessor is good for BOTH reading and writing. FYI - there is also a attr_writer. If you use this, the class attribute will be writable but not readable, which wouldn't make too much sense. You are willing to let someone write an attribute from outside the class but not read it from outside the class.]
+
+    # def gender=(new_gender)
+    #   @gender = new_gender
+    # end
+
+
+
+#DRIVER CODE::
+
+santas = Santa.new("women","black")
+puts "#Santa is a #{santas.ethnicity} #{santas.gender}."
+santas.get_mad_at("Vixen")
+santas.celebrate_birthday
+santas.gender = "Biologically female but identifies as male"
+puts "Santa's gender is: #{santas.gender}"
 
 
 # santas = []
 # example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-# example_genders.length.times do |i|
-#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# example_ethnicities = ["Black", "Latino", "Native American", "White", "Japanese-African", "prefer not to say"]
+
+# biological_gender.length.times do |i|
+#   santas << Santa.new(biological_gender[i], eye_color[i], hair_color[i])
 # end
 
-p santas
+
+
