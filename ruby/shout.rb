fd66b422d8fb4286608e0df9e81a53601460b209
Diana <diasandia@users.noreply.github.com>
2017-06-15 15:04:03 -0700

    Finish release 0 & 1 per assignment 6.4

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,2 +1,15 @@
 #6.4 Modules
 
+module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :( "
+  end
+
+  def self.yelling_happily(words)
+    words + "!!!" + "Happy, Happy!" + " :) "
+  end
+end
+
+#Driver Code
+p Shout.yell_angrily("No")
+p Shout.yelling_happily("HELLO")
