da9a9401502d24fa1a291abedef050a3dcacd3ba
Diana <diasandia@users.noreply.github.com>
2017-07-09 23:01:29 -0700

    Create table and add sales person method

--- databases/sales_quota/sales.rb
+++ databases/sales_quota/sales.rb
@@ -1,3 +1,21 @@
 require 'sqlite3'
 require 'faker'
 
+db = SQLite3::Database.new("sales.db")
+db.results_as_hash = true
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS sales(
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    quota INT
+  )
+SQL
+
+db.execute(create_table_cmd)
+
+def create_sales_person(db, first_name, last_name, quota)
+  db.execute("INSERT INTO sales (first_name, last_name, quota) VALUES (?, ?, ?)", [first_name, last_name, quota])
+end
+
